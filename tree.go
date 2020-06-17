8922e12f2b7f20fd46c1cfc83fe9552e38f0f649
sdwbgn <sdwbgn@gmail.com>
2020-06-17 15:31:49 +0900

    Finished main Finished printDirTree Finished printBranches Finished generateDirTree

--- tree.go
+++ tree.go
@@ -1,6 +1,9 @@
 package main
 
-import "io/ioutil"
+import (
+	"flag"
+	"io/ioutil"
+)
 
 type DirTreeNode struct {
 	elements []DirTreeNode
@@ -20,7 +23,7 @@ func generateDirTree(dir string, name string, dirOnly bool) DirTreeNode {
 	for _, fInfo := range files {
 		if fInfo.IsDir() {
 			val.elements = append(val.elements, generateDirTree(dir+"/"+fInfo.Name(), fInfo.Name(), dirOnly))
-		} else {
+		} else if !dirOnly {
 			val.elements = append(val.elements, DirTreeNode{
 				elements: nil,
 				name:     fInfo.Name(),
@@ -30,22 +33,47 @@ func generateDirTree(dir string, name string, dirOnly bool) DirTreeNode {
 	return val
 }
 
-func printBranches(branches []bool, lastChars string) {
-	for key, i := range branches {
-		if key+1 == len(branches) {
+func printBranches(branches *[]bool, lastChars string) {
+	for key, br := range *branches {
+		if key+1 == len(*branches) {
 			print(lastChars)
-		} else if i {
-			print("|   ")
+		} else if br {
+			print("│   ")
 		} else {
 			print("    ")
 		}
 	}
 }
 
-func printDirTree(node DirTreeNode, branches []bool) {
-
+func printDirTree(node DirTreeNode, branches *[]bool) {
+	println(node.name)
+	if branches == nil {
+		branches = new([]bool)
+	}
+	*branches = append(*branches, true)
+	for key, file := range node.elements {
+		if key+1 == len(node.elements) {
+			printBranches(branches, "└── ")
+			(*branches)[len(*branches)-1] = false
+		} else {
+			printBranches(branches, "├── ")
+		}
+		printDirTree(file, branches)
+	}
+	*branches = (*branches)[:len(*branches)-1]
 }
 
 func main() {
-
+	dirOnly := flag.Bool("d", false, "Show only directories")
+	flag.Parse()
+	dir := ""
+	if flag.NArg() == 0 {
+		dir = "."
+	} else if flag.NArg() == 1 {
+		dir = flag.Arg(0)
+	} else {
+		flag.PrintDefaults()
+		return
+	}
+	printDirTree(generateDirTree(dir, dir, *dirOnly), nil)
 }
